23511b171173f371fc5d9f098822aaada862fae6
Dyson <taodongsheng@onebest.cn>
2017-05-12 13:39:24 +0800

    增加自定义字段

---
+++ data/cache/apps.php
@@ -0,0 +1,29 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'VOcabDev',
+    'url' => 'http://vocab.dev',
+    'authkey' => '654321',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '0',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+);
+
+?>
---
+++ data/logs/201705.php
@@ -0,0 +1,12 @@
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494483983	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494484367	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494484893	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494488518	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494488599	user_add	username=xuesheng
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494490201	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494552756	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494552873	user_add	username=xuesheng
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494555132	login	succeed
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494556729	app_add	appid=1; appname=VOcabDev
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494558691	user_add	username=xuesheng
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1494558792	user_add	username=xuesheng
